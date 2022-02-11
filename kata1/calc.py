9a8e9a9d802023314582bd0fa16d15aaaab43355
Luis Martinez <leduardo.mp@gmail.com>
2022-02-10 18:40:50 -0600

    Kata 0 y 1

---
+++ kata1/calc.py
@@ -0,0 +1,5 @@
+print("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+#print(first_number + second_number)
+print(int(first_number) + int(second_number))
